d5f2cb62cf6af2451e0e74572335f9ecfec934da
Luis Saiete <luistsaiete@gmail.com>
2020-06-09 00:15:39 +0200

    adding cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333);
+app.listen(4000);
